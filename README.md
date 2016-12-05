32640023e327d66a4afebcc99167948fad15b8c6
mohankumaranna <mohankumar_anna@hotmail.com>
2016-12-05 11:46:52 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# apt-maintenance-account-server
+
+Apartment Maintenance Account Tracking App in Angular 2, Node JS, MySQL
---
+++ knexfile.js
@@ -0,0 +1,39 @@
+// Update with your config settings.
+
+require('dotenv').config();  // loads key-values in .env file into process.env
+
+module.exports = {
+
+  development: {
+    client: 'mysql',
+    connection: {
+      database: process.env.dev_database,
+      user: process.env.dev_user,
+      password: process.env.dev_password
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'mysql',
+    connection: {
+      database: process.env.prod_database,
+      user: process.env.prod_user,
+      password: process.env.prod_password
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }  
+
+};
