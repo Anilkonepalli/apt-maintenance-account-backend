9bef6019626745967370d89efb91423f8cb6088f
mohankumaranna <mohankumar_anna@hotmail.com>
2017-02-15 12:20:06 +0530

    impl auth

--- authorization.js
+++ authorization.js
@@ -21,14 +21,62 @@ module.exports = {
 	},
 
 	allowsEdit: function(userId, resource, model) {
-		return new Promise( function(resolve, reject) {
+		return allows(userId, resource, model, 'U');
+/*		return new Promise( function(resolve, reject) {
 			allows(userId, resource, model.owner, 'U'); // U stands for Update/Edit 
 			1 == 0 ? resolve(model) : reject(new Error('Unauthorized Access!!'));
-		});
+		}); */
 	}
 
 }
 
+function allows(userId, resource, model, action) {
+	
+	console.log('authorization process...');
+
+	return new Promise( function(resolve, reject) {
+		getUserPermissions(userId, resource)
+			.then(models => {
+				// find permissions with granted 'action'
+				let permissions = models.filter(perm => {
+					return perm.operations.indexOf(action) > -1;
+				});
+				let pCount = permissions.length;
+
+				// if no permissions found, then reject it with error message
+				if(pCount < 1) reject(new Error('Unauthorized Access!!'));
+
+				// find permissions with condition
+				let permissionsWithCondition = permissions.filter(perm => {
+					return perm.condition != null && perm.condition != '';
+				});
+				let pwcCount = permissionsWithCondition.length;
+
+				// if permission(s) exist but has no condition, pass the model for futher processing
+				if(pwcCount < 1) resolve(model);
+
+				// permissions with no condition take precedence, hence pass the model for futher processing 
+				if(pCount > pwcCount) resolve(model); 
+
+				// evaluate condition in each of the permissionsWithCondition
+				let fn;
+				let data;
+				let evaluatedPerms = permissionsWithCondition.filter(perm => { 	// filter for permission that 
+					fn = new Function("data", perm.condition);					// evaluates its condition to true
+					data = { userId: userId, ownerId: model.owner };
+					return fn(data);
+				});
+				evaluatedPerms.length > 0 
+					? resolve(model) // if at least one condition is evaluated to true, pass the model for futher processing
+					: reject(new Error('Unauthorized Access!!!')); // all conditions evaluated to false, return error with message
+			})
+			.catch(err => reject(err));
+	});
+	
+} 
+
+
+/*
 function allows(userId, resource, ownerId, action) {
 	console.log('authorization process...');
 	getUserPermissions(userId, resource)
@@ -58,4 +106,4 @@ function allows(userId, resource, ownerId, action) {
 			return evaluatedPerms.length > 0;
 		})
 		.catch(err => reject(err));	
-}
+} */
