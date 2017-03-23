4b7fe39882c9b51723920ee98f2d088eccbfaa09
mohankumaranna <mohankumar_anna@hotmail.com>
2017-03-23 10:31:08 +0530

    successfully added facebook login

--- authentication/social-login-routes.js
+++ authentication/social-login-routes.js
@@ -1,7 +1,6 @@
 var _ 			= require('lodash');
 var	jwt			= require('jsonwebtoken');
 var	bcrypt 	= require('bcrypt');
-//var hello 	= require('hello/dist/hello.all.js');
 var request = require('superagent');
 
 var	User 		= require('../users/user-model');
@@ -22,50 +21,49 @@ function createSession(request, response){
 	if( !network || !socialToken){
 		return response.status(400).send("Network name and Social token are needed");
 	}
-	validateWithProvider(network, socialToken) 	// calling inner function1
-		.then(checkUserAlreadyExist)							// calling inner function2
-		.then(getNewOrExistingUser)								// calling inner function3
-		.then(sendJwt)														// calling inner function4
-		.catch(sendError);												// calling inner function5
+	validateWithProvider(network, socialToken)
+		.then(checkUserAlreadyExist)							// calling inner function1
+		.then(getNewOrExistingUser)								// calling inner function2
+		.then(sendJwt)														// calling inner function3
+		.catch(sendError);												// calling inner function4
 
-	function checkUserAlreadyExist(userProfile){ // implementing inner function2
-console.log('checkUserAlreadyExist...');
-		profile = userProfile;
-		return User.where('email', profile.email).count('id');
-	}
-	function getNewOrExistingUser(count) { // implementing inner function3
-		console.log('Count is '+count);
-		if(count){
-			return User.forge({email: profile.email}).fetch();
-		} else {
-			return new User({
-				name: profile.name,
-				social_network_id: profile.id,
-				social_network_name: network,
-				email: profile.email
-			}).save();
-		}
-	}
-	function sendJwt(model) {	// implementing inner function4
-		console.log('doSendJwt...');console.log(model);
-			let user = model.toJSON();
-			let omitList = [
-				'password', 	'confirmed',	 'confirmation_code',
-				'created_at',	'updated_at',	 'deleted_at'
-			];
-			logger.log('info', 'Login through social network...');
-			return response.status(201).send({
-				id_token: jwt.sign(_.omit(user, omitList), constants.secret, {expiresIn: '2h', issuer: 'Apt-Maint'})
-			});
-	}
-	function sendError(err){ // implementing inner function5
-		response.status(500).json({error: true, data: {message: err.message}});
-	}
-
+			function checkUserAlreadyExist(userProfile){ // implementing inner function1
+				logger.log('info', 'checkUserAlreadyExist(..)');
+				profile = userProfile;
+				return User.where('email', profile.email).count('id'); // returns Promise containing count
+			}
+			function getNewOrExistingUser(count) { // implementing inner function2
+				logger.log('info', 'Count is '+count);
+				if(count){ // if count > 0, it means user exists in the system
+					return User.forge({email: profile.email}).fetch(); // returns Promise containing user model
+				} else { // if count is 0, then create new user and save it into system
+					return new User({ // returns Promise containing new user model
+						name: profile.name,
+						social_network_id: profile.id,
+						social_network_name: network,
+						email: profile.email
+					}).save();
+				}
+			}
+			function sendJwt(model) {	// implementing inner function3
+					logger.log('info', 'sendJwt(..)...Login through social network...');
+					let user = model.toJSON();
+					let omitList = [
+						'password', 	'confirmed',	 'confirmation_code',
+						'created_at',	'updated_at',	 'deleted_at'
+					];
+					return response.status(201).send({
+						id_token: jwt.sign(_.omit(user, omitList), constants.secret, {expiresIn: '2h', issuer: 'Apt-Maint'})
+					});
+			}
+			function sendError(err){ // implementing inner function4
+				logger.log('error', err.message);
+				response.status(500).json({error: true, data: {message: err.message}});
+			}
 }
 
-function validateWithProvider(network, socialToken) { // implementing inner function1
-console.log('validateWithProvider...');
+function validateWithProvider(network, socialToken) {
+	logger.log('info', 'validateWithProvider...');
 	return new Promise(function(resolve, reject) {
 		// Send a GET request to Facebook with the token as query string
 		request
@@ -74,7 +72,6 @@ console.log('validateWithProvider...');
 			.query({ "access_token": socialToken })
 			.end(function(error, response){
 				if( !error && response.statusCode == 200) {
-console.log('validateWithProvider...response..'); console.log(response.body);
 					resolve(response.body);
 				} else {
 					reject(error);
