c650f951587207a6ec106c6cef846177cf82ec96
mohankumaranna <mohankumar_anna@hotmail.com>
2017-04-02 14:28:38 +0530

    added unique constraints in Flats table

--- migrations/20170401154601_flats_table_creation.js
+++ migrations/20170401154601_flats_table_creation.js
@@ -5,13 +5,14 @@ exports.up = function(knex, Promise) {
 
   	return knex.schema.createTable('flats', function(table){
   		table.increments().primary();
-  		table.string('block_number', 20).defaultTo(''); // empty string means no block number exists
+  		table.string('block_number', 20).notNull().defaultTo('0'); // empty string means no block number exists
   		table.string('flat_number', 20).notNull();
   		table.string('remarks', 254).nullable();
       table.integer('owner_id').defaultTo(0); // owner id as 0 indicates admin only access
 		  table.timestamp('created_at').nullable().defaultTo(knex.fn.now());
 		  table.timestamp('updated_at').nullable();
 		  table.timestamp('deleted_at').nullable();
+      table.unique(['block_number', 'flat_number']);
   	});
 
 };
