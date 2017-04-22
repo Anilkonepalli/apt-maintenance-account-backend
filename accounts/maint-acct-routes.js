bdd38a6dfb05c8cadf78d09cd2f9ae866ebed8d3
mohankumaranna <mohankumar_anna@hotmail.com>
2017-04-22 11:57:25 +0530

    implementing balance calc

--- accounts/maint-acct-routes.js
+++ accounts/maint-acct-routes.js
@@ -125,52 +125,87 @@ console.log('model to be saved...')		; console.log(req.body);
 			remarks: req.body.remarks
 		}).save();
 	}
+
+/*
+
+function updateBalance(model){
+
+	//console.log('Saved posted account...'); console.log(model);
+	jmodel = model.toJSON();
+	console.log('jmodel is...'); console.log(jmodel);
+	MaintenanceAccounts
+		.fetch()
+		.then(processAll)
+		.catch(sendError);
+
+		function processAll(models) {
+			calculateBalanceAndApply(models, jmodel); // external function
+			return new Promise(function(resolve, reject){
+				1 > 0 ? resolve(model) : reject(new Error('Cannot calc balance'));
+			});
+		}
+
+		function sendError(err) {
+			logger.log('error', err.message);
+			return res.status(500).json({error: true, data: {message: err.message}});
+		}
+
+}
+
+ */
+
 	function updateBalance(model){
 
 		//console.log('Saved posted account...'); console.log(model);
 		jmodel = model.toJSON();
 		console.log('jmodel is...'); console.log(jmodel);
+		jmodels=[];
+
 		MaintenanceAccounts
-			.forge()
+			.query('where', 'recorded_at', '=', jmodel.recorded_at)
+			.orderBy('id', 'ASC')
 			.fetch()
-			.then(sortIterateApplyBalance)
+			.then(getModelsBeforeRecordDate)
+			.then(getModelsAfterRecordDate)
+			.then(processAll)
+			.then(sendResponse)
 			.catch(sendError);
 
-			// sort on fetched models
-			// find index of the model in the sorted models
-			// get balance from index - 1 model, use it to calculate balance for remaining
-			// models in the sorted list
-			function sortIterateApplyBalance(models) {
+			function getModelsBeforeRecordDate(models) {
+				jmodels = jmodels.concat( models.toJSON() );
+console.log('jmodels On Record Date'); console.log(jmodels);
+				return MaintenanceAccounts
+					.query('where', 'recorded_at', '<', jmodel.recorded_at)
+					.orderBy('recorded_at', 'DESC')
+					.orderBy('id', 'DESC')
+					.fetchOne();
+			}
 
-				let sortedModels = _.sortBy(models.toJSON(), [
-						function(model){ return model.recorded_at; }, // sort criteria 1
-						function(model){ return model.id;	}						// sort criteria 2
-					]);
-console.log('update balance: sorted models...'); console.log(sortedModels);
-				let indx = _.findIndex(sortedModels, function(o){
-console.log('o.recorded_at: '+o.recorded_at+', jmodel: '+jmodel.recorded_at);
-					return o.id == jmodel.id ;
-				});
-
-console.log('Index: '+indx);
-				let currentBalance = sortedModels[indx-1].balance; // previous account record's balance
-				currentBalance = currentBalance?currentBalance:0; // if null, set it to 0
-				for(i=indx; i < sortedModels.length; i++){
-					let acct = sortedModels[i];
-					currentBalance = currentBalance + acct.amount;
-console.log('Current Balance: '+currentBalance);
-					acct.balance = currentBalance;
-					//acct.save();
-					new MaintenanceAccount({id: jmodel.id})
-						.save({balance: currentBalance}, {patch: true})
-						.then( (model) => {
-							let obj = model.toJSON();
-							console.log('Model id '+obj.id+' is updated with balance: '+obj.balance);
-						})
-						.catch( (err) => {
-							console.error('Error while updating account with balance: '+err.message);
-						});
+			function getModelsAfterRecordDate(models) {
+				if(models){  // if models is not null
+					jmodels = jmodels.concat( models.toJSON() );
+console.log('jmodels Before Record Date'); console.log( models.toJSON() );
+				} else {
+console.log('no models before record date');					
 				}
+
+				return MaintenanceAccounts
+					.query('where', 'recorded_at', '>', jmodel.recorded_at)
+					.orderBy('recorded_at', 'ASC')
+					.orderBy('id', 'ASC')
+					.fetch();
+			}
+
+			function processAll(models) {
+				jmodels = jmodels.concat( models.toJSON() );
+console.log('jmodels After Record Date'); console.log( models.toJSON() );
+				calculateBalanceAndApply(jmodels, model);
+				return new Promise(function(resolve, reject){
+					1 > 0 ? resolve(true) : reject(new Error('Cannot calc balance'));
+				});
+			}
+
+			function sendResponse(models) {
 				return new Promise(function(resolve, reject){
 					1 > 0 ? resolve(model) : reject(new Error('Cannot calc balance'));
 				});
@@ -187,6 +222,55 @@ console.log('Current Balance: '+currentBalance);
 	}
 	function sendError(err) {
 		return res.status(500).json({error: true, data: {message: err.message}});
+	}
+}
+
+// private function
+// Sorts the models first, to start applying balance
+// find index of the jmodel if supplied;
+// get balance from index - 1 model, use it to calculate balance for
+// remaining models in the sorted list
+// Note: If jmodel is null, then balance
+// will start from first element in the sortedModels;
+function calculateBalanceAndApply(models, jmodel=NULL) {
+console.log('No. of models fetched...'); console.log(models.length);
+	//let jmodels = models.toJSON();
+	let jmodels = models;
+console.log('fetched models are: '); console.log(jmodels);
+
+	let sortedModels = _.sortBy(jmodels, [
+			function(model){ return model.recorded_at; }, // sort criteria 1
+			function(model){ return model.id;	}						// sort criteria 2
+		]);
+
+	let indx = jmodel
+		? _.findIndex(sortedModels, (o) => o.id === jmodel.id)
+		: 0; // if null, set indx to 0
+
+console.log('indx for assessing current balance = '+indx);
+	let currBal = 0;
+	if(indx > 0) { // if jmodel is not the first element in the sortedModels
+		bal = sortedModels[indx-1].balance; // previous account record's balance
+		currBal = bal?bal:0; // if bal is null, then set it to 0
+	}
+
+	for(i=indx; i < sortedModels.length; i++){ // calculate new balance and apply
+
+		let acct = sortedModels[i];
+
+		if(acct.crdr === 'cr'){ currBal = currBal + acct.amount; }
+		else if (acct.crdr === 'dr') {  currBal = currBal - acct.amount; }
+		else { throw new Error('Invalid crdr found');	}
+
+		new MaintenanceAccount({id: acct.id})
+			.save({balance: currBal}, {patch: true}) // save the balance into database
+			.then( (model) => {
+				let obj = model.toJSON();
+				console.log('Model id '+obj.id+' is updated with balance: '+obj.balance);
+			})
+			.catch( (err) => {
+				console.error('Error while updating account with balance: '+err.message);
+			});
 	}
 }
 
