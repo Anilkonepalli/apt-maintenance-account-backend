f5d098e72b8547adf66c22e5dbf9c9a5fa1d7c23
mohankumaranna <mohankumar_anna@hotmail.com>
2017-04-29 13:40:34 +0530

    fetching periodic list

--- accounts/maint-acct-periodic-routes.js
+++ accounts/maint-acct-periodic-routes.js
@@ -7,43 +7,70 @@ var MaintenanceAccounts = Bookshelf.Collection.extend({
 	model: MaintenanceAccount
 });
 
-
-// more routes for the API will happen here
-
+var	Flat 				= require('../flats/flat-model');
+var Flats 			= Bookshelf.Collection.extend({
+										model: Flat
+									});
 
 // on routes that end in /maintenance-accounts-periodic/
 // ---------------------------------------------------------------------
 function getAll(req, res) {
-// console.log('Request param is: '); console.log(req);
+
+	let availableModels = [];
+	let jmodels = [];
+	let flats = [];
+	let jflats = [];
+
 	MaintenanceAccounts
 		.query('where', 'for_month', '=', +req.query.month)
 		.query('where', 'for_year', '=', +req.query.year)
-		//.forge()
 		.fetch()
 		.then(doAuth)
+		.then(getFlats)
+		.then(fillMissingModels)
 		.then(sendResponse)
 		.catch(sendError);
 
 	function doAuth(models) {
+		availableModels = models;
+		jmodels = models.toJSON();
 		logger.log('info', '/api/maintenance-accounts-periodic >> getPeriodicList('+req.query.month+', '+req.query.year+')' );
 		return auth.allowedList(req.decoded.id, 'accounts', models);
 	}
+	function getFlats(models) {
+		return Flats.forge().fetch();
+	}
+	function fillMissingModels(flatz) {
+		flats = flatz;
+		jflats = flatz.toJSON();
 
+		let flatNumbers = jmodels.map(e => e.flat_number);
+		let acct;
+		let today = new Date().toISOString().split('T')[0]; // only consider date string and ignore time portion
+
+		jflats.forEach(e => {
+			if ( !flatNumbers.includes(e.flat_number) ) { // check for missing flat number
+				acct = new MaintenanceAccount().toJSON();
+				acct.flat_number = e.flat_number;
+				acct.for_month = req.query.month;
+				acct.for_year = req.query.year;
+				acct.recorded_at = today;
+				jmodels.push(acct);
+			}
+		});
+		return new Promise((resolve) => resolve(jmodels));
+	}
 	// sends Account models after sorting; sorting is based on its recorded_at field and then id field
-	function sendResponse(models) {
-		let sortedModels = _.sortBy(models.toJSON(), [
-				function(model){ return model.recorded_at; }, // sort criteria 1
-				function(model){ return model.id;	}						// sort criteria 2
+	function sendResponse(jmodels) {
+		let sortedModels = _.sortBy(jmodels, [
+				function(model){ return model.flat_number; }
 			]);
 		res.json(sortedModels);
 	}
-
 	function sendError(err) {
 		logger.log('error', err.message);
 		return res.status(500).json({error: true, data: {message: err.message}});
 	}
-
 }
-
 
 module.exports = { getAll };
