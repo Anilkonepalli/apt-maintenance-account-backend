fb5207dd141e1ab10dfdcdbf862e14c838311663
mohankumaranna <mohankumar_anna@hotmail.com>
2017-05-29 10:11:54 +0530

    owner_id field added into roles and permissions tables

--- migrations/20170128095224_permissions_table_creation.js
+++ migrations/20170128095224_permissions_table_creation.js
@@ -9,11 +9,12 @@ exports.up = function(knex, Promise) {
       table.string('condition', 180).nullable(); // function(params) { return params.user.id === params.post.id}
       table.unique(['operations', 'resource', 'condition']);
   		table.string('description', 255).nullable();
+			table.integer('owner_id').defaultTo(0); // owner_id as 0 indicates admin only access; here owner means who entered this record
 		  table.timestamp('created_at').nullable().defaultTo(knex.fn.now());
 		  table.timestamp('updated_at').nullable();
 		  table.timestamp('deleted_at').nullable();
   	});
-  
+
 };
 
 exports.down = function(knex, Promise) {
