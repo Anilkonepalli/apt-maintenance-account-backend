1945ee75c4483b778abdff2a5320c6ab9f04033d
mohankumaranna <mohankumar_anna@hotmail.com>
2017-06-11 16:08:23 +0530

    yet to identify invalid jwt in handling google api

--- authentication/oauth2.js
+++ authentication/oauth2.js
@@ -5,8 +5,10 @@ var fs = require('fs');
 var aptMaint = fs.readFileSync('./AptMaint-9d84488d54cc.json');
 var aptMaintJson = JSON.parse(aptMaint);
 
-var now = new Date();
-var timenow = now.getSeconds();
+var today = new Date();
+var todayUTC = today.toUTCString();
+var todaySeconds = Math.floor( today.getTime() / 1000 ) - 300;
+var expiry = todaySeconds + 600;
 
 let payload = {
   // JWT Headers
@@ -16,19 +18,19 @@ let payload = {
   // JWT Claim set
   "iss": aptMaintJson.client_email,
   "scope": "https://mail.google.com/",
-  "aud": "https://www.googleapis.com/oauth2/v4/token"
+  "aud": "https://www.googleapis.com/oauth2/v4/token",
+  "iat": todaySeconds
 };
 
 let cert = aptMaintJson.private_key;
 
-let jwtToken = jwt.sign(payload, cert, { expiresIn: timenow + 600 }); // expires in 600 seconds
+let jwtToken = jwt.sign(payload, cert, { expiresIn: expiry }); // expires in 600 seconds
+//let jwtToken = jwt.sign(payload, cert); // expires in 600 seconds
 
 function createJWT() {
-  // console.log('AptMaintJSON...'); console.log(aptMaintJson);
-  // console.log('Issuer: ...'); console.log(aptMaintJson.client_email);
-  // console.log('Private Key:...'); console.log(cert);
-  console.log('Date now: '); console.log(now);
-  console.log('Time now: '); console.log(timenow);
+  console.log('Today: '+today);
+  console.log('Today UTC: '+todayUTC);
+  console.log('todaySeconds: '+todaySeconds);
   console.log('jwtToken...'); console.log(jwtToken);
 }
 
