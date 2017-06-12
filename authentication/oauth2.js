7eb34c15b5d12633bab506b18e62700607c91340
mohankumaranna <mohankumar_anna@hotmail.com>
2017-06-12 13:57:41 +0530

    impl get access token

--- authentication/oauth2.js
+++ authentication/oauth2.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 var aptMaint = fs.readFileSync('./AptMaint-9d84488d54cc.json');
 var aptMaintJson = JSON.parse(aptMaint);
 
+/*
 var today = new Date();
 var todayUTC = today.toUTCString();
 var todaySeconds = Math.floor( today.getTime() / 1000 ) - 300;
@@ -19,13 +20,14 @@ let payload = {
   "iss": aptMaintJson.client_email,
   "scope": "https://mail.google.com/",
   "aud": "https://www.googleapis.com/oauth2/v4/token"
-};
+}; */
 
-let payload2 = {
+let payload = {
   "scope": "https://mail.google.com/"
 };
 
 let cert = aptMaintJson.private_key;
+let url = "https://www.googleapis.com/oauth2/v4/token";
 
 let options = {
   algorithm: 'RS256',
@@ -35,13 +37,19 @@ let options = {
 }
 
 //let jwtToken = jwt.sign(payload, cert, { expiresIn: expiry }); // expires in 600 seconds
-let jwtToken = jwt.sign(payload2, cert, options); // expires in 600 seconds
+let jwtToken = jwt.sign(payload, cert, options); // expires in 600 seconds
 
 function createJWT() {
-  console.log('Today: '+today);
-  console.log('Today UTC: '+todayUTC);
-  console.log('todaySeconds: '+todaySeconds);
   console.log('jwtToken...'); console.log(jwtToken);
+}
+function getAccessToken() {
+  https.post(url, (res) => {
+    res.on('data', (d) => {
+      console.log(d);
+    });
+  }).on('error', (e) => {
+    console.error(e);
+  });
 }
 
 module.exports = { createJWT };
