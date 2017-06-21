1dcd1c11d30b912b84d9183d79eb8816cf9cf306
mohankumaranna <mohankumar_anna@hotmail.com>
2017-06-21 09:45:33 +0530

    updating seeds

--- seeds/sample_permissions.js
+++ seeds/sample_permissions.js
@@ -9,35 +9,149 @@ exports.seed = function(knex, Promise) {
       return Promise.all([
         // Inserts seed entries
         knex('permissions').insert({
-                        operations: 'R',
-                        resource: 'accounts',
-                        condition: null,
-                        description: 'Grants permissions to read/view Account records'
-                      }),
+                          operations: 'R',
+                          resource: 'accounts',
+                          condition: NULL,
+                          description: 'Grants READ ONLY permissions to ANY USERS'
+                        }),
         knex('permissions').insert({
-                        operations: 'CRUD',
-                        resource: 'users',
-                        condition: '(params)=>{return params.user.id === params.loggedUser.id;}',
-                        description: 'Grants permissions to create/read/update/delete own user profile'
-                      }),
+                          operations: 'RUD',
+                          resource: 'users',
+                          condition: 'return data.user_id === data.model.owner_id',
+                          description: 'Grants RUD PERMISSIONS to user\'s OWN RECORDS'
+                        }),
         knex('permissions').insert({
-                        operations: 'CRUD',
-                        resource: 'accounts',
-                        condition: null,
-                        description: 'Grants permissions to create/read/update/delete any account record'
-                      }),
+                          operations: 'CRUD',
+                          resource: 'accounts',
+                          condition: NULL,
+                          description: 'Grants ALL PERMISSIONS to ANY USERS'
+                        }),
         knex('permissions').insert({
-                        operations: 'UD',
-                        resource: 'accounts',
-                        condition: '(params)=>{return params.user.id === params.account.owner_id;}',
-                        description: 'Grants permissions to update/delete own account record'
-                      }),
+                          operations: 'R',
+                          resource: 'accounts',
+                          condition: 'return data.user_id == data.model.owner_id',
+                          description: 'Grants READ ONLY permissions to USER SPECIFIC record'
+                        }),
         knex('permissions').insert({
-                        operations: 'CRUD',
-                        resource: 'roles',
-                        condition: null,
-                        description: 'Grants permissions to create/read/update/delete any role'
-                      }),
+                          operations: 'CRUD',
+                          resource: 'roles',
+                          condition: NULL,
+                          description: 'Grants ALL PERMISSIONS to ANY USERS'
+                        }),
+        knex('permissions').insert({
+                          operations: 'CRUD',
+                          resource: 'flats',
+                          condition: NULL,
+                          descriptioN: 'Grants ALL PERMISSIONS to ANY USERS'
+                        }),
+        knex('permissions').insert({
+                          operations: 'CRUD',
+                          resource: 'residents',
+                          condition: NULL,
+                          description: 'Grants ALL PERMISSIONS to ANY USERS'
+                        }),
+        knex('permissions').insert({
+                          operations: 'CRUD',
+                          resource: 'users',
+                          condition: NULL,
+                          description: 'Grants ALL PERMISSIONS to ANY USERS'
+                        }),
+        knex('permissions').insert({
+                          operations: 'R',
+                          resource: 'users',
+                          condition: NULL,
+                          description: 'Grants READ ONLY permission to ANY USERS'
+                        }),
+        knex('permissions').insert({
+                          operations: 'R',
+                          resource: 'flats',
+                          condition: NULL,
+                          description: 'Grants READ ONLY permission to ANY USERS'
+                        }),
+        knex('permissions').insert({
+                          operations: 'R',
+                          resource: 'residents',
+                          condition: NULL,
+                          description: 'Grants READ ONLY permission to ANY USER'
+                        }),
+        knex('permissions').insert({
+                          operations: 'R',
+                          resource: 'roles',
+                          condition: NULL,
+                          description: 'Grants READ ONLY permission to ANY USERS'
+                        }),
+        knex('permissions').insert({
+                          operations: 'CRUD',
+                          resource: 'permissions',
+                          condition: NULL,
+                          description: 'Grants ALL PERMISSIONS to ANY USERS'
+                        }),
+        knex('permissions').insert({
+                          operations: 'R',
+                          resource: 'permissions',
+                          condition: NULL,
+                          description: 'Grants READ ONLY permission to ANY USERS'
+                        }),
+        knex('permissions').insert({
+                          operations: 'RU',
+                          resource: 'flats-residents',
+                          condition: NULL,
+                          description: 'Grants READ and UPDATE permissions to ANY USERS'
+                        }),
+        knex('permissions').insert({
+                          operations: 'R',
+                          resource: 'flats-residents',
+                          condition: NULL,
+                          description: 'Grants READ ONLY permission to ANY USERS'
+                        }),
+        knex('permissions').insert({
+                          operations: 'RU',
+                          resource: 'users-roles',
+                          condition: NULL,
+                          description: 'Grants READ and UPDATE permissions to ANY USERS'
+                        }),
+        knex('permissions').insert({
+                          operations: 'R',
+                          resource: 'users-roles',
+                          condition: NULL,
+                          description: 'Grants READ ONLY permission to ANY USERS'
+                        }),
+        knex('permissions').insert({
+                          operations: 'RU',
+                          resource: 'roles-permissions',
+                          condition: NULL,
+                          description: 'Grants READ and UPDATE permissions to ANY USERS'
+                        }),
+        knex('permissions').insert({
+                          operations: 'R',
+                          resource: 'roles-permissions',
+                          condition: NULL,
+                          description: 'Grants READ ONLY permission to ANY USERS',
+                        }),
+        knex('permissions').insert({
+                          operations: 'D',
+                          resource: 'accounts',
+                          condition: NULL,
+                          description: 'Grants DELETE permission to ANY USERS'
+                        }),
+        knex('permissions').insert({
+                          operations: 'CRU',
+                          resource: 'accounts',
+                          condition: NULL,
+                          description: 'Grants ALL but Delete permissions to ANY USERS'
+                        }),
+        knex('permissions').insert({
+                          operations: 'R',
+                          resource: 'accounts',
+                          condition: 'return params.user_id === params.model.owner_id',
+                          description: 'Grants READ ONLY permissions to USER SPECIFIC record'
+                        }),
+        knex('permissions').insert({
+                          operations: 'RU',
+                          resource: 'user-profile',
+                          condition: 'return data.user_id === data.model.owner_id',
+                          description: 'Grants RU permissions to USER\'s OWN RECORD'
+                        })
       ]);
     });
 };
