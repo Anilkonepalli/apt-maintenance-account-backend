fe99ef7a1d492fc191194c3ba77f14e66694eb65
Mohan <mohankumar.anna@outlook.com>
2017-11-29 20:05:04 +0530

    fixed problem in seed file

--- seeds/sample_2_maint_accts.js
+++ seeds/sample_2_maint_accts.js
@@ -1,5 +1,6 @@
 // [ item, flat_number, for_month, for_year, name, crdr, amount, category, recorded_at, remarks ]
 let today = new Date();
+let tDate = today.toISOString().slice(0, 10); // get date string alone
 let year = today.getFullYear();
 let month = today.getMonth() + 1; // month is from 0 to 11 hence +1 is added
 
@@ -12,17 +13,17 @@ if(month3 > 12){ month3 = 1; year3++;}
 
 
 let sampleData = [
-  ['test item1', 'G1', month1, year1, 'tester1', 'cr', 600.00, 'Monthly maintenance', today, 'testing maint app'],
-  ['test item2', 'G2', month1, year1, 'tester2', 'dr', 100.00, 'Monthly maintenance', today, 'testing maint app'],
-  ['test item3', 'G3', month1, year1, 'tester3', 'cr', 600.00, 'Monthly maintenance', today, 'testing maint app'],
+  ['test item1', 'G1', month1, year1, 'tester1', 'cr', 600.00, 'Monthly maintenance', tDate, 'testing maint app'],
+  ['test item2', 'G2', month1, year1, 'tester2', 'dr', 100.00, 'Monthly maintenance', tDate, 'testing maint app'],
+  ['test item3', 'G3', month1, year1, 'tester3', 'cr', 600.00, 'Monthly maintenance', tDate, 'testing maint app'],
 
-  ['test item4', 'G1', month2, year2, 'tester4', 'dr', 200.00, 'Monthly maintenance', today, 'testing maint app'],
-  ['test item5', 'G2', month2, year2, 'tester5', 'cr', 600.00, 'Monthly maintenance', today, 'testing maint app'],
-  ['test item6', 'G3', month2, year2, 'tester6', 'dr', 200.00, 'Monthly maintenance', today, 'testing maint app'],
+  ['test item4', 'G1', month2, year2, 'tester4', 'dr', 200.00, 'Monthly maintenance', tDate, 'testing maint app'],
+  ['test item5', 'G2', month2, year2, 'tester5', 'cr', 600.00, 'Monthly maintenance', tDate, 'testing maint app'],
+  ['test item6', 'G3', month2, year2, 'tester6', 'dr', 200.00, 'Monthly maintenance', tDate, 'testing maint app'],
 
-  ['test item7', 'G1', month3, year3, 'tester7', 'cr', 600.00, 'Monthly maintenance', today, 'testing maint app'],
-  ['test item8', 'G2', month3, year3, 'tester8', 'dr', 300.00, 'Monthly maintenance', today, 'testing maint app'],
-  ['test item9', 'G3', month3, year3, 'tester9', 'cr', 600.00, 'Monthly maintenance', today, 'testing maint app']
+  ['test item7', 'G1', month3, year3, 'tester7', 'cr', 600.00, 'Monthly maintenance', tDate, 'testing maint app'],
+  ['test item8', 'G2', month3, year3, 'tester8', 'dr', 300.00, 'Monthly maintenance', tDate, 'testing maint app'],
+  ['test item9', 'G3', month3, year3, 'tester9', 'cr', 600.00, 'Monthly maintenance', tDate, 'testing maint app']
 
 ];
 let tableName = "maintenance_accounts";
